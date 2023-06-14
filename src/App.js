603c107fadbb49504d784b2c341d1e53198010c2
youssef tadlaoui <usftadlaoui@gmail.com>
2023-06-14 17:26:30 +0100

    FC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  return(
+  <div className="steps">
+    <div className="numbers">
+        <div >
+          1
+        </div>
+        <div>
+          2
+        </div>
+        <div>
+          3
+        </div>
     </div>
-  );
+    <p className="message">Hello</p>
+    <div className="buttons">
+      <button style={{backgroundColor:'#7950F2',color:'#fff'}}>Previous</button>
+      <button style={{backgroundColor:'#7950F2',color:'#fff'}}>Next</button>
+    </div>
+  </div>
+  )
 }
-
 export default App;
